e267ec2dd25a8390c387ce2da0549dbee0b4b276
Pixelastic <tim@pixelastic.com>
2018-10-25 12:59:54 +0200

    Add watch aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,47 +1,52 @@
 # Generic faster aliases
 alias k='kubectl'
+alias kR='k delete'
 alias kc='kubectl apply -f'
+alias kga='kg all'
 
 # Pods
 alias kpl='kube-pod-list'
 alias kplw='kube-pod-list-watch'
+alias kpR='kR pod'
+alias kpRa='kR pod --all'
+alias kps='kube-pod-ssh'
 # Deployments
 alias kdl='kube-deployment-list'
 alias kdlw='kube-deployment-list-watch'
+alias kdR='kR deployment'
+alias kdRa='kR deployment --all'
 # Services
 alias ksl='kube-service-list'
 alias kslw='kube-service-list-watch'
+alias ksR='kR service'
+alias ksRa='kR service --all'
 # Jobs
 alias kjl='kube-job-list'
 alias kjlw='kube-job-list-watch'
+alias kjR='kR job'
+alias kjRa='kR job --all'
 
 
 
 
 
-alias kR='k delete'
 alias kg='k get -o wide'
 alias kgv='kg -o json'
 alias kgw='kg --watch'
 alias kRa='kR --all'
-alias kga='kg all'
 alias kgaw="watch 'kubectl get all'"
 # Kind-agnostic
 alias kl='kubectl logs'
 # Pods
 alias kpRa='kR pod --all'
-alias kpR='kR pod'
 alias kpv='kgv pod'
 # Deployment
 alias kdRa='kR deployment --all'
-alias kdR='kR deployment'
 # Services
 alias ksRa='kR service --all'
-alias ksR='kR service'
 alias ksv='kgv service'
 # Ingresses
 alias kiRa='kR ingress --all'
-alias kiR='kR ingress'
 alias kiv='kgv ingress'
 
 
